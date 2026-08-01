6b0d1ecee3efbd75e1c4916a57f697f3f2d52f16
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-08-01 23:08:15 +0530

    Update Enums.cs

--- WaterApp.Domain/Enums/Enums.cs
+++ WaterApp.Domain/Enums/Enums.cs
@@ -21,7 +21,8 @@ public enum OrderStatus
     Confirmed = 1,
     OutForDelivery = 2,
     Delivered = 3,
-    Cancelled = 4
+    Cancelled = 4,
+    PendingPayment = 5
 }
 
 public enum PaymentMode
